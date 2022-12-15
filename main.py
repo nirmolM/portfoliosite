1988afcdee6c9a04e922507436dfa52f18f57b51
Nirmol's PC <nirmol882@gmail.com>
2022-12-15 13:51:16 +0530

    Content Added

--- main.py
+++ main.py
@@ -12,6 +12,16 @@ with col2:
     content = """
     Hi, I am Nirmol! I am a Professor, Design and Verification Engineer, Cloud Architect, Python Programmer and a Teacher.
     I graduated in 2019 with a Masters in Technology and have worked with Shah and Anchor Kutchhi Engineering College as 
-    an Assistant Professor. I am willing to Purse a PhD in a Computer Science Domain   
+    an Assistant Professor. I am willing to Purse a PhD in a Computer Science Domain
+    My Area of Interests are 
+    RTL Design and Verification, SoC Architecture, Physical Design,
+    Cloud Architecture, Server Administration, Enterprise Network Design,
+    Data Science, Data Mining, Warehousing, Python Desktop, Web and Game Development   
     """
     st.info(content)
+
+content2 = """
+Below you can find some of the Apps and Projects I have Built. Feel free to Contact me!
+"""
+
+st.write(content2)
